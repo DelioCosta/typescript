601eb810f0d534c2a9eab16a773c67ad18dc58c1
Délio <deliocqcosta@gmail.com>
2022-05-31 19:26:54 -0300

    Primeiro exercício de TypeScript. Criada função para somar dois numeros

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+const meuNumero1 = 25;
+const meuNumero2 = 8;
+const resultado = somar(meuNumero1, meuNumero2);
+console.log(resultado);
